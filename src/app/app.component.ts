6aceb1da6e4714b432051756adfc0877a637d1eb
thomas <cardonneth@eisti.eu>
2021-03-07 18:10:28 +0100

    avancement menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import{ModalController}from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(public modalController:ModalController) {}
+
+  showSubmenu: boolean = false;
+  showSubmenu2: boolean = false;
+
+
+menuItemHandler(): void {
+  this.showSubmenu = !this.showSubmenu;
+}
+menuItemHandler2(): void {
+  this.showSubmenu2 = !this.showSubmenu2;
+}
 }
